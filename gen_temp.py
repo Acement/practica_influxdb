ffdc6963a72f02f497dfbb2ba29b5599c6de51a6
Acement <pj.cisterna@gmail.com>
2024-12-31 12:57:00 -0300

    Agregada subida con time stamps simulados

---
+++ gen_temp.py
@@ -0,0 +1,51 @@
+import json, random
+
+with open("config.json",'r') as f:
+    data = json.load(f)
+
+gen_data = data["gen_config"]
+ext_data = data["idb_config"]
+
+def main():
+    open_time   = 60 * (gen_data["hora_cierre"] - gen_data["hora_apertura"]) #tiempo abierto en minutos
+    temperature = gen_data["temperatura"]
+
+    simulated_temp = []
+    for i in range(0,open_time):
+        temperature = round(temperature + ((random.randint(-5 + gen_data["temp_cambio"],5 + gen_data["temp_cambio"]))/10),2)
+        if temperature > gen_data["temp_max"]:
+            temperature = gen_data["temp_max"]
+        elif temperature < gen_data["temp_min"]:
+            temperature = gen_data["temp_min"]
+        simulated_temp.append(temperature)
+
+    hour      = gen_data["hora_apertura"]
+    minute    = 0
+    json_data = []
+
+    for i in range(0,open_time):
+        if minute < 10:
+            print(f"Hora: {hour}:0{minute}, Temperatura: {simulated_temp[i]}")
+            temp = [f"{hour}:0{minute}",simulated_temp[i]]
+        else:
+            print(f"Hora: {hour}:{minute}, Temperatura: {simulated_temp[i]}")
+            temp = [f"{hour}:{minute}",simulated_temp[i]]
+
+        json_data.append(temp)
+
+        minute += 1
+
+        if minute == 60:
+            minute = 0
+            hour   +=1
+
+    with open("temperature.json",'r') as f:
+        data = json.load(f)
+
+    data["temperature"] = json_data
+
+    with open("temperature.json",'w') as f:
+        json.dump(data,f,indent= 2)
+
+if __name__ == "__main__":
+    main()
